f7e247b7d9f398ab1672691ed4b30657cdc71110
dongkx <dongkangxin@outlook.com>
2019-12-03 15:19:34 +0800

    框架搭建

---
+++ boot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#获取当前路径
+cd ./target
+jar_name="Venus-*.*-SNAPSHOT.jar"
+case "$1" in
+
+  start)
+    mvn clean
+    mvn install
+    nohup java -jar $(jar_name) &
+    tail -f nohup.out
+    ;;
+
+  stop)
+    cur_pid=grep $(jar_name)|grep -v grep|awk '{print $2}'
+    kill -9 $(cur_pid)
+    ehco "$(jar_name) stop success"
+    ;;
+
+  restart)
+    $0 stop
+    sleep 3
+    $0 start
+    ;;
+
+  esac
+
+
+
+
+
